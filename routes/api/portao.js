6039dbd6d9bdd0dba9800c9e438181872611e605
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2021-03-08 10:23:36 -0300

    nova versao

--- routes/api/portao.js
+++ routes/api/portao.js
@@ -18,7 +18,6 @@ router.get("/test", function (req, res) {
 });
 
 router.get("/list", async (req, res) => {
-  const id = req.params.id;
   const pg = req.query.pg||0;
 
   Portao.find({  })
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,21 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: req.params.nome || "Sem nome" });
 });
 router.get("/portao/list", function (req, res, next) {
-  res.render("portao/lista", { pageTitle: req.params.nome || "Sem nome" });
+  const pg = req.query.pg || 0;
+
+  Portao.find({})
+    .limit(10)
+    .skip(pg * 10)
+    .then((docs) =>{
+      console.log(docs);
+      return res.render("portao/lista", {docs })
+    });
+    
 });
 router.get("/portao/qrcode/:id", function (req, res, next) {
   const id = req.params.id;
   Portao.findOne({ _id: new mongoose.Types.ObjectId(id) }).then((docs) =>
-    res.render("portao/qrcode", { ...docs._doc })
+    res.render("portao/qrcode", { ...docs._doc, removeNav: true })
   );
 });
 
