d3a8e1562b4175ab4dcd9368400b0a645ee3f8e9
lucianovilela <lucianovilela@gmail.com>
2022-03-23 20:32:40 +0000

    WIP

--- src/services/db.js
+++ src/services/db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-var uri = `mongodb://admin:${process.env.DB_PASSWORD}@cluster0-shard-00-00.mrzgo.mongodb.net:27017,cluster0-shard-00-01.mrzgo.mongodb.net:27017,cluster0-shard-00-02.mrzgo.mongodb.net:27017/portaoeletronico?ssl=true&replicaSet=atlas-k6rsc8-shard-0&authSource=admin&retryWrites=true&w=majority`;
+var uri = process.env.URI_DATABASE;
 // const uri = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.mrzgo.mongodb.net/portaoeletronico?retryWrites=true&w=majority`;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
